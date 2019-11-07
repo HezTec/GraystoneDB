7776bb9310b5fbbd815c755cc7b2521b30d1a14f
hackerrank <17colemanm@gmail.com>
2019-11-07 09:11:52 -0500

    Moved object creation classes to Control package, deleted useless class files

---
+++ src/GraystoneControl/User.java
@@ -0,0 +1,27 @@
+package GraystoneControl;
+/*
+ * Employee Connection to Database
+ * 
+ */
+public class User {
+
+	private String userId;
+	private String password;
+	
+	public User(String userId, String password) {
+		super();
+		this.userId = userId;
+		this.password = password;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
